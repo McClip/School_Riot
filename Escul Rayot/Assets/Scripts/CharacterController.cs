1c79c0d634f271c7cde2fb44ff78de84a7d00aae
TobyDX <107975375+McClip@users.noreply.github.com>
2022-07-06 12:13:05 -0400

    bug

---
+++ Escul Rayot/Assets/Scripts/CharacterController.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterController : MonoBehaviour
+{
+    //variables:
+    Rigidbody2D Rb;
+    public SpriteRenderer Sr;
+    public float velocidad = 2;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.D)) 
+        {
+            Rb.velocity = new Vector2(velocidad,Rb.velocity.y);
+            Sr.flipX = false;
+        }
+        else if (Input.GetKey(KeyCode.A))
+        {
+            Rb.velocity = new Vector2(-velocidad, Rb.velocity.y);
+            Sr.flipX = true;
+        }
+        else
+        {
+            Rb.velocity = new Vector2(0, Rb.velocity.y);
+        }
+    }
+}
